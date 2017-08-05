3139143f9b0ecb3c65d57bc7863a5c4851f50874
Joe Frew <joefrew@protonmail.com>
2017-08-05 13:14:54 -0400

    app level added

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,12 +3,12 @@ var path = require("path")
 module.exports = function(app){
 
 	app.get("/tables", function(req, res){
-		res.sendFile(path.join(__dirname, "../../public/tables.html"));
+		res.sendFile(path.join(__dirname, "../../app/public/tables.html"));
 	});
 	app.get("/reserve", function(req, res){
-		res.sendFile(path.join(__dirname, "../../public/reserve.html"));
+		res.sendFile(path.join(__dirname, "../../app/public/reserve.html"));
 	});
 	app.use(function(req, res){
-		res.sendFile(path.join(__dirname, "../../public/home.html"));
+		res.sendFile(path.join(__dirname, "../../app/public/home.html"));
 	});
 }

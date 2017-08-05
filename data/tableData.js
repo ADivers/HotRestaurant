5e67d303b2abab15e9577d2deac62c7166b64d5d
mmegarba <micaeljohnmegarbane@gmail.com>
2017-08-05 11:34:48 -0400

    data files added

---
+++ data/tableData.js
@@ -0,0 +1,17 @@
+var tableData = [
+
+{
+	customerName: "Ahmed",
+    customerEmail: "ahmed@example.com",
+    customerID: "afhaque89",
+    phoneNumber: "000-000-0000"
+
+}
+
+
+
+
+]
+
+
+module.exports(tableData);

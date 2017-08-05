8032ff63dd4acead365000fd39b22eed2a9019f7
Joe Frew <joefrew@protonmail.com>
2017-08-05 13:00:34 -0400

    fixed routing location

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,25 +6,25 @@ switch(urlParts.pathname){
 
 	case "/tables":
 	app.get("/tables", function(req, res){
-		res.sendFile(path.join(__dirname, "../public/tables.html"));
+		res.sendFile(path.join(__dirname, "../../public/tables.html"));
 	});
 	break;
 
 	case "/reserve":
 	app.get("/reserve", function(req, res){
-		res.sendFile(path.join(__dirname, "../public/reserve.html"));
+		res.sendFile(path.join(__dirname, "../../public/reserve.html"));
 	});
 	break;
 
 	case "/home":
 	app.get("/home", function(req, res){
-		res.sendFile(path.join(__dirname, "../public/home.html"));
+		res.sendFile(path.join(__dirname, "../../public/home.html"));
 	});
 	break;
 
 	default:
 	app.get("/404", function(req, res){
-		res.sendFile(path.join(__dirname, "../public/404.html"));
+		res.sendFile(path.join(__dirname, "../../public/404.html"));
 	});
 	break;
 }

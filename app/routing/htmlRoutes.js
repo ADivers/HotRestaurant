a7744cf4af12a89ae23c21f41538da7712bc08d5
Joe Frew <joefrew@protonmail.com>
2017-08-05 13:09:11 -0400

    more things

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,31 +1,15 @@
 var path = require("path")
-var url = require("url")
+
 module.exports = function(app){
 
-switch(urlParts.pathname){
-
-	case "/tables":
 	app.get("/tables", function(req, res){
 		res.sendFile(path.join(__dirname, "../../public/tables.html"));
 	});
-	break;
-
-	case "/reserve":
 	app.get("/reserve", function(req, res){
 		res.sendFile(path.join(__dirname, "../../public/reserve.html"));
 	});
-	break;
-
-	case "/home":
-	app.get("/home", function(req, res){
+	app.use(function(req, res){
 		res.sendFile(path.join(__dirname, "../../public/home.html"));
 	});
-	break;
-
-	default:
-	app.get("/404", function(req, res){
-		res.sendFile(path.join(__dirname, "../../public/404.html"));
-	});
-	break;
 }
 }
